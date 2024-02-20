c857df3f06eacb197f80697eb5ea397b144bd268
Admin <admin@Admins-MacBook-Pro-8.local>
2024-02-20 12:39:52 +0530

    subtraction feature added

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,7 @@ Class Calculator{
         System.out.println(num1+num2);
     }
     void subtraction(it num1, int num2){
-
+        System.out.println(num1-num2);
     }
     public static void main(String[] args){
         Scanner scanner=new Scanner();
