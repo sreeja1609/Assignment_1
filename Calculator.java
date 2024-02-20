d059b21e052c06e1798571cf7dd3dee24ee8b90f
Admin <admin@Admins-MacBook-Pro-8.local>
2024-02-20 11:59:34 +0530

    Initial commit of basic calculator file

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+import java.util.*;
+Class Calculator{
+    void addition(int num1, int num2){
+
+    }
+    void subtraction(it num1, int num2){
+        
+    }
+    public static void main(String[] args){
+        Scanner scanner=new Scanner();
+        int num1, num2;
+        System.out.println("Input two numbers for performing mathematical operations");
+        num1=scanner.nextInt();
+        num2=scanner.nextInt();
+        addition(num1, num2);
+        subtraction(num1, num2);
+    }
+}
